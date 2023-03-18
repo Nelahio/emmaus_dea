36e5bab348986d2d57cac5c4f9da872b248a1561
Nelahio <57316380+Nelahio@users.noreply.github.com>
2023-03-18 09:29:32 +0100

    Update page_declaration.dart
    suppression de la connexion à la bdd

--- lib/pages/page_declaration.dart
+++ lib/pages/page_declaration.dart
@@ -2,51 +2,10 @@ import 'package:emmaus_dea/class/colors_app.dart';
 import 'package:emmaus_dea/widgets/Declaration/CardDeclaration.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_expandable_fab/flutter_expandable_fab.dart';
-import 'package:mysql_client/mysql_client.dart';
 
 class PageDeclaration extends StatelessWidget {
   const PageDeclaration({Key? key}) : super(key: key);
-
-  Future<void> _query() async {
-    await dotenv.load();
-    print("Connecting to mysql server...");
-
-    // create connection
-    final conn = await MySQLConnection.createConnection(
-      host: dotenv.env['DB_HOST']!,
-      port: int.parse(dotenv.env['DB_PORT']!),
-      userName: dotenv.env['DB_USER']!,
-      password: dotenv.env['DB_PASSWORD']!,
-      databaseName: dotenv.env['DB_DATABASE']!, // optional
-    );
-
-    await conn.connect();
-
-    print("Connected");
-
-    // make query
-    var result = await conn.execute("SELECT * FROM meuble");
-
-    // print some result data
-    print(result.numOfColumns);
-    print(result.numOfRows);
-    print(result.lastInsertID);
-    print(result.affectedRows);
-
-    // print query result
-    for (final row in result.rows) {
-      // print(row.colAt(0));
-      // print(row.colByName("title"));
-
-      // print all rows as Map<String, String>
-      print(row.assoc());
-    }
-
-    // close all connections
-    await conn.close();
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -72,9 +31,8 @@ class PageDeclaration extends StatelessWidget {
             backgroundColor: ColorsApp.Blue_Color,
             icon: const Icon(Icons.add_card_rounded),
             label: Text("Fiche"),
-            onPressed: () async {
+            onPressed: () {
               if (kDebugMode) {
-                await _query();
                 print("Fiche");
               }
             },
